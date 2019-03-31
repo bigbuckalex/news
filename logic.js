b4b20d0b7b2549afb700ea6dc61c038fad2dcbaf
bigbuckalex <bigbuckalex@gmail.com>
2019-03-31 15:49:17 -0400

    css styling

--- logic.js
+++ logic.js
@@ -1,24 +1,16 @@
-// let query = document.getElementById("search").value;
-
-// console.log(query);
-
 $(document).on("keypress", "input", function(e){
     if(e.which == 13){
         let search = $(this).val();
-        let url = 'https://newsapi.org/v2/everything?' +
-        'q='+search+'&' +
-        'apiKey=8236dbb2862a466183c933236963c805';
+        let url = 'https://newsapi.org/v2/everything?'+'q='+search+'&language=en&sortBy=publishedAt'+'&apiKey=8236dbb2862a466183c933236963c805';
 
         $.get(url, function(res, status){
             let articles = res.articles;
-            for(let i=0; i<articles.length; i++){
-                $("#news-feed").append("<h3>"+articles[i].title+"</h3><h6>"+articles[i].source.name+"</h6><p>"+articles[i].description+"</p><a class='btn btn-primary' href='"+articles[i].url+"' target='_blank' role='button'>Full Story</a></div>");
+            console.log(articles);
+            for(let i=articles.length-1; i>=0; i--){
+                $("#news-feed").prepend("<h2>"+articles[i].title+"</h2><h6>"+articles[i].source.name+"</h6><br><p>"+articles[i].description+"</p><a class='btn btn-primary' href='"+articles[i].url+"' target='_blank' role='button'>Full Story</a></div><br><br>");
             }
+        }).then(function(){
+            console.log("done");
         });
     }
 });
-
-// axios.get("https://newsapi.org/v2/everything?q=" + query + "&sortBy=publishedAt&apiKey=8236dbb2862a466183c933236963c805")
-//     .then((res)=>{
-
-//     })
