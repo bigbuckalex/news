ece9eacf5fe766a1ea512c1bd984bfa614d4e854
bigbuckalex <bigbuckalex@gmail.com>
2019-03-31 16:44:08 -0400

    changed max number of articles to 100

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
 $(document).on("keypress", "input", function(e){
     if(e.which == 13){
         let search = $(this).val();
-        let url = 'https://newsapi.org/v2/top-headlines?'+'q='+search+'&language=en&sortBy=publishedAt'+'&apiKey=8236dbb2862a466183c933236963c805';
+        let url = 'https://newsapi.org/v2/top-headlines?'+'q='+search+'&pageSize=100&language=en&sortBy=publishedAt'+'&apiKey=8236dbb2862a466183c933236963c805';
 
         $.get(url, function(res, status){
             let articles = res.articles;
@@ -9,7 +9,7 @@ $(document).on("keypress", "input", function(e){
             for(let i=articles.length-1; i>=0; i--){
                 $("#news-feed").prepend("<div class='separator'></div><br><h2>"+articles[i].title+"</h2><h6>"+articles[i].source.name+"</h6><br><p>"+articles[i].description+"</p><a class='btn btn-primary' href='"+articles[i].url+"' target='_blank' role='button'>Full Story</a></div><br>");
             }
-            $("#num-articles").text("Found "+articles.length+" articles about "+search);
+            $("#num-articles").text('Found '+articles.length+' articles about "'+search+'"');
         }).then(function(){
             console.log("done");
         });
